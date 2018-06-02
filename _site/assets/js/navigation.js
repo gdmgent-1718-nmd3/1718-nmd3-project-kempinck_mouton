010219748d3057d143f0350bf6ab347cc0b47e76
yasvkemp <yasvkemp@student.artevelde.be>
2018-06-02 15:01:09 +0200

    [WIP]

--- _site/assets/js/navigation.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  const activeClass = 'active';
-  const $navLinks = $('.gdm-nav .nav-link:not(.disabled)');
-  $navLinks.on('click', function () {
-    $navLinks.removeClass(activeClass);
-    $(this).addClass(activeClass);
- })
-});
